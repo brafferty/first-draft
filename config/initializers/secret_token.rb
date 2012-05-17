388bde81e74baecc4e079dd0f691dbea0b31f108
Brian Bostwick <brafferty@gmail.com>
2012-05-17 16:22:23 -0400

    Create rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectgym::Application.config.secret_token = '66c157d0ddd405cdf9fe7dfdc04eee0a9eacf5ce1016e61dc55d7f8b48b841337e3da36a92064f614833046aaadd965711b69f6e5e5c1eafc6424de761045596'
